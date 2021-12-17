486539a944056b3c5419e9de41cdbd25c3462fab
Daisy <daisy.v.quadros@gmail.com>
2021-12-17 15:47:32 -0500

    INFO-6205 submission by 001568823 - Daisy quadros - section1

--- src/main/java/LargestTree.java
+++ src/main/java/LargestTree.java
@@ -1,23 +1,38 @@
-import java.util.*;
-class LargestTree{
-    static void addEdge(LinkedList<Integer> adj[], int u, int v)
-    {
-        //TO-DO:
+import java.util.LinkedList;
+import java.util.Vector;
+
+class LargestTree {
+
+    static void addEdge(LinkedList<Integer>[] adj, int u, int v) {
+        adj[u].add(v);
+        adj[v].add(u);
     }
 
-    static int DFS(int u, LinkedList<Integer> adj[], Vector<Boolean> visited)
-    {
+    static int DFS(int u, LinkedList<Integer>[] adj, Vector<Boolean> visited) {
         visited.add(u, true);
         int size = 1;
-
-        // Iterate through all the nodes and perform DFS if the node is not yet visited
-        //TO-DO:
-        return 0;
+        for (int i = 0; i < adj[u].size(); i++)
+            if (!visited.get(adj[u].get(i)))
+                size += DFS(adj[u].get(i),
+                        adj, visited);
+        return size;
     }
 
-    public int largestTree(LinkedList<Integer> adj[], int V)
-    {
-        //TO-DO:
-        return 0;
+    public int largestTree(LinkedList<Integer>[] adj, int V) {
+        Vector<Boolean> visited = new Vector<>();
+        int resultant = 0;
+
+        for (int i = 0; i < V; i++) {
+            visited.add(false);
+        }
+
+        for (int u = 0; u < V; u++) {
+            if (!visited.get(u)) {
+                resultant = Math.max(resultant,
+                        DFS(u, adj, visited));
+            }
+        }
+        return resultant;
     }
+
 }
